dcb44a2c8d8dc4b557aaa9e1ba5224444adca3d4
adamtaylorsmith <adamtaylor@icloud.com>
2022-03-19 23:30:43 -0400

    logotype

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Paper, TextField } from '@material-ui/core'
+// import logo from './../images/logo'
 
 class SearchBar extends React.Component {
     
@@ -19,11 +20,16 @@ class SearchBar extends React.Component {
     render(){
         return (
             <>
+                <img src={require('./../images/logo.gif')} />
                 <Paper elevation={6} style={{ padding: '25px' }}>
+                    
                     <form onSubmit={this.handleSubmit}>
                         <TextField fullWidth label="Search..." onChange={this.handleChange}></TextField>
                     </form>
                 </Paper>
+                {/* <img src={require('./../images/first.gif')} />
+                <img src={require('./../images/noti.gif')} />
+                <img src={require('./../images/icon.gif')} /> */}
             </>
         )
     }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -8,7 +8,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
     return (
         <>
             <Grid item xs={10}>
-                <Paper style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={() => onVideoSelect(video)}>
+                <Paper style={{ display: 'flex', alignItems: 'center', cursor: 'pointer', padding: '10px' }} onClick={() => onVideoSelect(video)}>
                     <img style={{ marginRight: '20px' }} alt="thumbnail" src={video.snippet.thumbnails.medium.url}  />
                     <Typography variant="subtitle1"><b>{video.snippet.title}</b></Typography>
                 </Paper>
